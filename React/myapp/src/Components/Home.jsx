a1f26e271fa50ae554961b9597b19c7a2255b1f8
A01198693 <isaacrojassosa@gmail.com>
2024-05-25 21:20:42 -0600

    mapa de

---
+++ React/myapp/src/Components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Home.module.css';
+
+const Home = () => {
+  return (
+    <div className={styles.view}>
+      <h1>Inicio</h1>
+      <p>Bienvenido a la página de inicio.</p>
+    </div>
+  );
+};
+
+export default Home;
---
+++ React/myapp/src/Components/Mas.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Mas.module.css';
+
+const Más = () => {
+  return (
+    <div className={styles.view}>
+      <h1>Más</h1>
+      <p>Más información.</p>
+    </div>
+  );
+};
+
+export default Más;
