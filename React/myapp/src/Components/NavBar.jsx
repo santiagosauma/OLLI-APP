af8885af88bdb9a86d0b1b7dfb4cddbbf30ec3e8
SantiagoSauma <lssaumap@gmail.com>
2024-05-26 08:23:02 -0600

    Fixed NavBar

--- React/myapp/src/Components/NavBar.jsx
+++ React/myapp/src/Components/NavBar.jsx
@@ -23,8 +23,8 @@ const Navbar = ({ setView }) => {
       <div className={styles.header_content}>
         <ul>
           <li className={currentView === 'Home' ? `${styles.active}` : ''} onClick={() => handleItemClick('Home')}>Inicio</li>
-          <li className={currentView === 'Ciudad' ? styles.active : ''} onClick={() => handleItemClick('Ciudad')}>Ciudad</li>
-          <li className={currentView === 'PrediccionEF' ? styles.active : ''} onClick={() => handleItemClick('PrediccionEF')}>Predicción EF</li>
+          <li className={currentView === 'Ciudad' ? styles.active : ''} onClick={() => handleItemClick('Ciudad')}>Predicción EF</li>
+          <li className={currentView === 'PrediccionEF' ? styles.active : ''} onClick={() => handleItemClick('PrediccionEF')}>Ciudad</li>
           <li className={currentView === 'NuevoLeon' ? styles.active : ''} onClick={() => handleItemClick('NuevoLeon')}>NuevoLeon</li>
           <li className={currentView === 'Nosotros' ? styles.active : ''} onClick={() => handleItemClick('Más')}>Nosotros</li>
           
